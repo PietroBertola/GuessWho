9778d87cfa13eabe61673f21dbd13c841002b216
CavalleroRoberto <96049331+CavalleroRoberto@users.noreply.github.com>
2022-12-06 09:28:07 +0100

    Aggiornamento

--- GuessWho/Form1.Designer.cs
+++ GuessWho/Form1.Designer.cs
@@ -31,14 +31,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtIp = new System.Windows.Forms.Label();
+            this.lblIp = new System.Windows.Forms.Label();
             this.lblStatoServer = new System.Windows.Forms.Label();
+            this.btnIni = new System.Windows.Forms.Button();
+            this.txtIp = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(735, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(44, 45);
@@ -56,16 +57,16 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
-            // txtIp
+            // lblIp
             // 
-            this.txtIp.AutoSize = true;
-            this.txtIp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.txtIp.Location = new System.Drawing.Point(318, 393);
-            this.txtIp.Name = "txtIp";
-            this.txtIp.Size = new System.Drawing.Size(174, 20);
-            this.txtIp.TabIndex = 2;
-            this.txtIp.Text = "XXXXXXXXXXXXXXX";
-            this.txtIp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblIp.AutoSize = true;
+            this.lblIp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblIp.Location = new System.Drawing.Point(318, 393);
+            this.lblIp.Name = "lblIp";
+            this.lblIp.Size = new System.Drawing.Size(174, 20);
+            this.lblIp.TabIndex = 2;
+            this.lblIp.Text = "XXXXXXXXXXXXXXX";
+            this.lblIp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblStatoServer
             // 
@@ -78,19 +79,39 @@
             this.lblStatoServer.Text = "STOPPED";
             this.lblStatoServer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // btnIni
+            // 
+            this.btnIni.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIni.ForeColor = System.Drawing.Color.OliveDrab;
+            this.btnIni.Location = new System.Drawing.Point(344, 314);
+            this.btnIni.Name = "btnIni";
+            this.btnIni.Size = new System.Drawing.Size(125, 33);
+            this.btnIni.TabIndex = 5;
+            this.btnIni.Text = "CONNETTI";
+            this.btnIni.UseVisualStyleBackColor = true;
+            this.btnIni.Click += new System.EventHandler(this.btnIni_Click);
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(310, 261);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(200, 22);
+            this.txtIp.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SkyBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblStatoServer);
+            this.Controls.Add(this.btnIni);
             this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.lblStatoServer);
+            this.Controls.Add(this.lblIp);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -104,8 +125,10 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label txtIp;
+        private System.Windows.Forms.Label lblIp;
         private System.Windows.Forms.Label lblStatoServer;
+        private System.Windows.Forms.Button btnIni;
+        private System.Windows.Forms.TextBox txtIp;
     }
 }
 
--- GuessWho/Form1.cs
+++ GuessWho/Form1.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Sockets;
+using System.Text.RegularExpressions;
 
 namespace GuessWho
 {
@@ -17,6 +18,7 @@ namespace GuessWho
     public partial class Form1 : Form
     {
         private clsSocket serverSocket;
+        Regex rgx = new Regex("^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
         public Form1()
         {
             InitializeComponent();
@@ -24,14 +26,14 @@ namespace GuessWho
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            Application.Exit();
             this.Close();
+            Application.Exit();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             clsAddress.cercaIP();
-            txtIp.Text= clsAddress.ipVett[0].ToString()+":8888";
+            lblIp.Text= clsAddress.ipVett[0].ToString()+":8080";
             IPAddress ip;
 
             if (serverSocket == null)
@@ -39,7 +41,7 @@ namespace GuessWho
                 ip = clsAddress.ipVett[0];
 
                 // Istanzio il Server Socket
-                serverSocket = new clsSocket(true,8888, ip);
+                serverSocket = new clsSocket(true,8080, ip);
 
                 serverSocket.datiRicevutiEvent += new datiRicevutiEventHandler(datiRicevuti);
             }
@@ -50,14 +52,50 @@ namespace GuessWho
         }
         private void datiRicevuti(clsMessaggio inMsg)
         {
-            aggiornaGraficaEventHandler pt = new aggiornaGraficaEventHandler(aggiornaGrafica);
-            this.Invoke(pt, inMsg);
+            DialogResult dr = MessageBox.Show("Vuoi giocare con "+inMsg.ToString()+"?",
+                      "Richiesta", MessageBoxButtons.YesNo);
+            switch (dr)
+            {
+                case DialogResult.Yes:
+                    aggiornaGraficaEventHandler pt = new aggiornaGraficaEventHandler(aggiornaGrafica);
+                    this.Invoke(pt, inMsg);
+                    break;
+                case DialogResult.No:
+                    break;
+            }
+
+            
         }
 
         private void aggiornaGrafica(clsMessaggio inMsg)
         {
             // Riporto il Messaggio ricevuto dal Client nella
             //    Lista dei Messaggi ricevuti sul Server
+        }
+
+        private void btnIni_Click(object sender, EventArgs e)
+        {
+            if (rgx.IsMatch(txtIp.Text) == true)
+            {
+                IPAddress ip;
+                clsSocket clientSocket;
+                clsMessaggio MsgFromServer;
+
+
+                ip = clsAddress.cercaIP(txtIp.Text);
+                clientSocket = new clsSocket(false, Convert.ToInt16("8080"), ip);
+                // Invio il Messaggio al SERVER
+                clientSocket.inviaMsg(lblIp.Text);
+
+                // Aspetto la Risposta del SERVER
+                MsgFromServer = clientSocket.clientRicevi();
+
+                // Facoltativo !!
+                MessageBox.Show("CONNESSO");
+
+            }
+            else
+                MessageBox.Show("nope");
         }
     }
 
